cde2231e099207f2cb742ba5e564e71695de90d7
saurabhd <saurabhdhariwal.com@gmail.com>
2016-02-04 20:39:07 +0530

    Design

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', 'root');
 
 /** MySQL database password */
 /* define('DB_PASSWORD', '110582'); */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 /* define('DB_HOST', 'localhost'); */
