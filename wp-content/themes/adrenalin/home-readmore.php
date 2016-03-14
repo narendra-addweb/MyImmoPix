5fab35ab84c88cfa49ab42b78dd0b6498e26240f
Narendra <narendra.addweb@gmail.com>
2016-03-14 12:13:00 +0530

    Add home page Readmore content as per GIT-89

---
+++ wp-content/themes/adrenalin/home-readmore.php
@@ -0,0 +1,46 @@
+<div class="col-lg-12 col-md-12 parent_readmore">
+	<?php if(ICL_LANGUAGE_CODE == 'en'){?>
+		<div class="readmore-left">
+			<h3>Real estate photography : a crucial impact !</h3>
+			<p>Real estate listings often suffer from sub-par photography.</p>
+			<p>High quality pictures are often the key to a quicker and more profitable deal.</p>
+			<p>Studies on this topic show that 60 % of the time spent looking at a listing is spent on the pictures. 76 % of readers move on to the description of a property after looking at the pictures. Quite often, the first picture is what triggers a potential buyers interest in a property.</p>
+			<p>It’s also essential that a reader put the listing in his list of « favorite » properties. The listing should leave a lasting impression in the buyers mind so that he or she will return to it after seeing the other listings.</p>
+		</div>
+		<div class="readmore-right">
+			<h3>Our appreciation of a place goes beyond what the eyes, and their judgement, can contribute.</h3>
+			<p>Sight is not the only sense we use to evaluate a place. Our body grasps a "feel" for a space that is just as important as what the eyes can contribute. When walk through rooms, halls and buildings our feeling is influenced by the dimensions and the choice of materials.</p>
+			<p>We feel whether a place is confining or allows us to breath comfortably. Are the proportions harmonious or strange ?</p>
+			<p>Our perception and our feel of a place is directly influenced by the space and it’s representation in photographs.</p>
+		</div>
+	<?php }else if(ICL_LANGUAGE_CODE == 'nl'){?>
+		<div class="readmore-left">
+			<h3>Real estate photography : a crucial impact !</h3>
+			<p>Real estate listings often suffer from sub-par photography.</p>
+			<p>High quality pictures are often the key to a quicker and more profitable deal.</p>
+			<p>Studies on this topic show that 60 % of the time spent looking at a listing is spent on the pictures. 76 % of readers move on to the description of a property after looking at the pictures. Quite often, the first picture is what triggers a potential buyers interest in a property.</p>
+			<p>It’s also essential that a reader put the listing in his list of « favorite » properties. The listing should leave a lasting impression in the buyers mind so that he or she will return to it after seeing the other listings.</p>
+		</div>
+		<div class="readmore-right">
+			<h3>Our appreciation of a place goes beyond what the eyes, and their judgement, can contribute.</h3>
+			<p>Sight is not the only sense we use to evaluate a place. Our body grasps a "feel" for a space that is just as important as what the eyes can contribute. When walk through rooms, halls and buildings our feeling is influenced by the dimensions and the choice of materials.</p>
+			<p>We feel whether a place is confining or allows us to breath comfortably. Are the proportions harmonious or strange ?</p>
+			<p>Our perception and our feel of a place is directly influenced by the space and it’s representation in photographs.</p>
+		</div>
+	<?php }else if(ICL_LANGUAGE_CODE == 'fr'){?>
+		<div class="readmore-left">
+			<h3>La photo immobilière : un impact décisif !</h3>
+			<p>La qualité des photos sur les annonces immobilières laisse souvent à désirer.</p>
+			<p>Elles sont pourtant le gage du succès d’une vente ou d’une location plus rapide et à meilleur prix.</p>
+			<p>Les études faites en ce sens démontrent que 60% du temps de consultation d’une annonce est passé sur les photos et 76% des lecteurs vont s’attarder sur la description après avoir vu les photos. Bien souvent la première photo est l’élément déclencheur de l’intérêt pour un bien.</p>
+			<p>Il est également essentiel que l’annonce soit retenue comme « favorite » et que, plus généralement, elle laisse une impression chez le lecteur afin d’y revenir au vu des autres annonces.</p>
+		</div>
+		<div class="readmore-right">
+			<h3>L’appréciation que nous portons sur un lieu va au delà de ce que les yeux – et leur jugement – procurent.</h3>
+			<p>Nous ne percevons pas seulement avec les yeux, mais avec le corps également. Le sens « tactile » a autant d’importance que le sens visuel.</p>
+			<p>Nous traversons les pièces, les couloirs et les bâtiments selon un ressenti que les matériaux et les dimensions nous donnent.</p>
+			<p>Nous percevons si nous pouvons respirer librement dans un espace ou au contraire y être suffoqués. Les proportions sont-elles harmonieuses ou étranges ?</p>
+			<p>Notre ressenti et perception répondent directement et immédiatement à une quelconque atmosphère émise par le lieu et ses représentations photographiques.</p>
+		</div>
+	<?php }?>		
+</div>
