78616cb2437fcaf21f5facbfc82f59a990ff70ae
vivek-addweb <vivek.addweb@gmail.com>
2016-03-18 16:07:57 +0530

    Design: solved GIT issue

--- wp-content/themes/adrenalin/src/js/custom.js
+++ wp-content/themes/adrenalin/src/js/custom.js
@@ -5,7 +5,29 @@ jQuery(document).ready(function() {
   jQuery('.input-rc').append('<span class="input-rc-span"></span>');
   /* End */
 
- /* Login Popup */
+  /* Custom select design */
+  jQuery('.custom-lang-select').append('<div class="button"></div>');
+  jQuery('.custom-lang-select').append('<ul class="select-list"></ul>');
+  //jQuery('.custom-lang-select .button').append('<a href="javascript:void(0);" class="select-list-link">Arrow</a>');
+
+  jQuery('.custom-lang-select select option').each(function() {
+    var bg = jQuery(this).css('background-image');
+    jQuery('.select-list').append('<li class="clsAnchor"><a href="'+jQuery(this).val()+'" class="'+ jQuery(this).attr('class') +'" style=background-image:' + bg + '>'+jQuery(this).text()+'</a></li>');
+  });
+  jQuery('.custom-lang-select .button').html(jQuery('.custom-lang-select select').find(':selected').text() + '<a href="javascript:void(0);" class="select-list-link">Arrow</a>');
+
+  jQuery('.custom-lang-select ul li').each(function() {
+    if (jQuery(this).find('a').text() == jQuery('.custom-lang-select select').find(':selected').text()) {
+      jQuery(this).addClass('active');
+    }
+  });
+
+  jQuery('.custom-lang-select a.select-list-link').click(function(){
+    jQuery('.custom-lang-select ul li').slideToggle();
+  });
+  /* End */
+
+  /* Login Popup */
   jQuery(".lwa .lwa-modal-bg").live( "click", function() {
     jQuery(".lwa .lwa-modal").hide();
   });
@@ -15,7 +37,7 @@ jQuery(document).ready(function() {
   });
   /* End */
 
-  /* Scrolling effect in home page*/
+   /* Scrolling effect in home page*/
   jQuery( "#menu-home-menus li a" ).live( "click", function() {
     var target = jQuery(this).attr("href");
       jQuery('html, body').animate({
@@ -30,29 +52,4 @@ jQuery(document).ready(function() {
       }, 1000);
   });
 
-  // Custom select design
-  jQuery('.custom-lang-select').append('<div class="button"></div>');
-  jQuery('.custom-lang-select').append('<ul class="select-list"></ul>');
-  //jQuery('.custom-lang-select .button').append('<a href="javascript:void(0);" class="select-list-link">Arrow</a>');
-  jQuery('.custom-lang-select select option').each(function() {
-    //alert('option');
-    var bg = jQuery(this).css('background-image');
-    //alert(bg);
-    jQuery('.select-list').append('<li class="clsAnchor"><a href="'+jQuery(this).val()+'" class="'+ jQuery(this).attr('class') +'" style=background-image:' + bg + '>'+jQuery(this).text()+'</a></li>');
-  });
-  jQuery('.custom-lang-select .button').html(jQuery('.custom-lang-select select').find(':selected').text() + '<a href="javascript:void(0);" class="select-list-link">Arrow</a>');
-  jQuery('.custom-lang-select ul li').each(function() {
-    if (jQuery(this).find('a').text() == jQuery('.custom-lang-select select').find(':selected').text()) {
-      jQuery(this).addClass('active');
-    }
-  });
-
-  jQuery('.custom-lang-select a.select-list-link').click(function(){
-    jQuery('.custom-lang-select ul li').slideToggle();
-  });
-
-  if(jQuery('.custom-lang-select select option').attr('selected') == 'selected') {
-
-  }
-  // End 
 });
