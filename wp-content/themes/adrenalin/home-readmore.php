cfb76a8eef15881c53ba0d88699f3589817a03ba
Narendra <narendra.addweb@gmail.com>
2016-03-18 13:51:06 +0530

    Changes as client given content for GIT-89

--- wp-content/themes/adrenalin/home-readmore.php
+++ wp-content/themes/adrenalin/home-readmore.php
@@ -16,17 +16,18 @@
 			</div>
 		<?php }else if(ICL_LANGUAGE_CODE == 'nl'){?>
 			<div class="readmore-left">
-				<h3>Real estate photography : a crucial impact !</h3>
-				<p>Real estate listings often suffer from sub-par photography.</p>
-				<p>High quality pictures are often the key to a quicker and more profitable deal.</p>
-				<p>Studies on this topic show that 60 % of the time spent looking at a listing is spent on the pictures. 76 % of readers move on to the description of a property after looking at the pictures. Quite often, the first picture is what triggers a potential buyers interest in a property.</p>
-				<p>It’s also essential that a reader put the listing in his list of « favorite » properties. The listing should leave a lasting impression in the buyers mind so that he or she will return to it after seeing the other listings.</p>
+				<h3>Vastgoed fotografie: een cruciale impact!</h3>
+				<p>Vastgoed advertenties komen vaak niet tot hun recht door minderwaardige foto’s.</p>
+				<p>Hoge kwaliteit foto’s zijn dikwijls de sleutel tot een snellere en meer rendabele verkoop.</p>
+				<p>Vanuit studies weten we dat 60% van de tijd die men spendeert aan het bekijken van de advertenties gaan naar de foto’s. 76% van de bezoekers kijken vervolgens verder naar de beschrijving van het pand.
+				Heel vaak is het de eerste foto die interesse opwekt bij mogelijke kopers.</p>
+				<p>Verder is het essentieel dat de bezoeker het pand opneemt in de lijst met favoriten. De advertentie moet dus een blijvende indruk nalaten bij de potentiele kopers zodat hij of zij terugkeert na het screenen van de andere advertenties.</p>
 			</div>
 			<div class="readmore-right">
-				<h3>Our appreciation of a place goes beyond what the eyes, and their judgement, can contribute.</h3>
-				<p>Sight is not the only sense we use to evaluate a place. Our body grasps a "feel" for a space that is just as important as what the eyes can contribute. When walk through rooms, halls and buildings our feeling is influenced by the dimensions and the choice of materials.</p>
-				<p>We feel whether a place is confining or allows us to breath comfortably. Are the proportions harmonious or strange ?</p>
-				<p>Our perception and our feel of a place is directly influenced by the space and it’s representation in photographs.</p>
+				<h3>Onze appreciatie voor een pand gaat verder dan wat onze ogen, en hun beoordeling, kunnen bijdragen.</h3>
+				<p>Het zicht is niet het enige zintuig dat we gebruiken om een locatie te evalueren. We worden een “gevoel” gewaar voor een bepaalde plaats, wat even belangrijk is dan de bijdrage van de ogen. Zo wordt het gevoel beïnvloed door de afmetingen en de keuze van materialen die men waarneemt tijdens een bezoek aan de verschillende ruimtes of gebouwen.</p>
+				<p>We voelen als het ware wanneer een ruimte beperkend of eerder bevrijdend werkt. Zijn de afmetingen harmonieus of eerder vreemd?</p>
+				<p>Onze perceptive en gevoel van een pand wordt onmiddelijk beïnvloed door de ruimte en hoe deze gepresenteerd wordt in de foto’s.</p>
 			</div>
 		<?php }else if(ICL_LANGUAGE_CODE == 'fr'){?>
 			<div class="readmore-left">
