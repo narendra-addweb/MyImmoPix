f9bb5b4c3fe3ddf0d14a6af09a5e89381dbd650f
Narendra <narendra.addweb@gmail.com>
2016-03-22 11:32:31 +0530

    Resolved GIT-101

---
+++ wp-content/themes/adrenalin/content-aftercart.php
@@ -0,0 +1,18 @@
+<?php $user_ID = get_current_user_id();?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="entry-content">
+        
+		<?php the_content(); ?>
+        <?php
+        wp_link_pages( array(
+            'before' => '<div class="page-links">' . __( 'Pages:', 'commercegurus' ),
+            'after' => '</div>',
+        ) );
+        ?>
+    </div><!-- .entry-content -->
+    <div class="container">
+        <?php edit_post_link( __( 'Edit', 'commercegurus' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+    </div>
+</article><!-- #post-## -->
+
+
