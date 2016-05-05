fbeb9093b2d62a7b1551d2f88e532dc4343a028a
vivek-addweb <vivek.addweb@gmail.com>
2016-05-05 16:10:48 +0530

    Changes in div struture for upload photo page

--- wp-content/themes/adrenalin/template-upload.php
+++ wp-content/themes/adrenalin/template-upload.php
@@ -27,25 +27,27 @@ if(!$user_ID)
    <div class="tunnelicon">
        
 	  	<div class="col-lg-12 col-md-12 tunnel">
-		<div class="col-lg-3 col-md-3 tunnel-iconimg"><img src="<?php echo $upload_dir['baseurl']; ?>/icon/upload_photos_active.png" width="100px" height="80px" /></div>
-		<div class="col-lg-3 col-md-3 tunnel-iconimg"><img src="<?php echo $upload_dir['baseurl']; ?>/icon/project_review_inactive.png" width="100px" height="80px" /></div>
-		<div class="col-lg-3 col-md-3 tunnel-iconimg"><img src="<?php echo $upload_dir['baseurl']; ?>/icon/order_summary_inactive.png" width="100px" height="80px" /></div>
-		<div class="col-lg-3 col-md-3 tunnel-iconimg"><img src="<?php echo $upload_dir['baseurl']; ?>/icon/order_confirmed_inactive.png" width="100px" height="80px" /></div>
+		<div class="col-lg-3 col-md-3 tunnel-iconimg">
+            <img src="<?php echo $upload_dir['baseurl']; ?>/icon/upload_photos_active.png" width="100px" height="80px" />
+            <span class="photo-upload-step">1 <small><?php echo get_str_uploadphoto1();?> </small></span>
+        </div>
+		<div class="col-lg-3 col-md-3 tunnel-iconimg">
+            <img src="<?php echo $upload_dir['baseurl']; ?>/icon/project_review_inactive.png" width="100px" height="80px" />
+            <span class="photo-upload-step">2 <small> <?php echo get_str_review_project();?></small></span>
+        </div>
+		<div class="col-lg-3 col-md-3 tunnel-iconimg">
+            <img src="<?php echo $upload_dir['baseurl']; ?>/icon/order_summary_inactive.png" width="100px" height="80px" />
+            <span class="photo-upload-step">3 <small><?php echo get_str_ordersummary();?></small></span>
+        </div>
+		<div class="col-lg-3 col-md-3 tunnel-iconimg">
+            <img src="<?php echo $upload_dir['baseurl']; ?>/icon/order_confirmed_inactive.png" width="100px" height="80px" />
+            <span class="photo-upload-step">4 <small><?php echo get_str_order_complete();?></small></span>
+        </div>
 		
 		</div>
 		
     </div>
-	
-	<div class="col-lg-12 col-md-12 tunnel">
-	<div class="col-lg-3 col-md-3"><span>1 <small><?php echo get_str_uploadphoto1();?> </small></span></div>
-	<div class="col-lg-3 col-md-3"><span>2 <small> <?php echo get_str_review_project();?></small></span></div>
-	<div class="col-lg-3 col-md-3"><span>3 <small><?php echo get_str_ordersummary();?></small></span></div>
-	<div class="col-lg-3 col-md-3"><span>4 <small>  <?php echo get_str_order_complete();?></small></span></div>
-	</div>
-   
-   
-   
-   
+
     <div class="content">
     
         <?php if(isset($_GET['pid']) &&  !empty($_GET['pid'])){?>
