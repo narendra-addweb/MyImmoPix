13a8d9db55cdb62aa5f683f8ce40f1f41f37dc01
vivek-addweb <vivek.addweb@gmail.com>
2016-05-05 16:40:44 +0530

    Changes in struture for upload photo page

--- wp-content/themes/adrenalin/template-upload.php
+++ wp-content/themes/adrenalin/template-upload.php
@@ -27,19 +27,19 @@ if(!$user_ID)
    <div class="tunnelicon">
        
 	  	<div class="col-lg-12 col-md-12 tunnel">
-		<div class="col-lg-3 col-md-3 tunnel-iconimg">
+		<div class="col-lg-3 col-sm-3 col-xs-6 tunnel-iconimg">
             <img src="<?php echo $upload_dir['baseurl']; ?>/icon/upload_photos_active.png" width="100px" height="80px" />
             <span class="photo-upload-step">1 <small><?php echo get_str_uploadphoto1();?> </small></span>
         </div>
-		<div class="col-lg-3 col-md-3 tunnel-iconimg">
+		<div class="col-lg-3 col-sm-3 col-xs-6 tunnel-iconimg">
             <img src="<?php echo $upload_dir['baseurl']; ?>/icon/project_review_inactive.png" width="100px" height="80px" />
             <span class="photo-upload-step">2 <small> <?php echo get_str_review_project();?></small></span>
         </div>
-		<div class="col-lg-3 col-md-3 tunnel-iconimg">
+		<div class="col-lg-3 col-sm-3 col-xs-6 tunnel-iconimg">
             <img src="<?php echo $upload_dir['baseurl']; ?>/icon/order_summary_inactive.png" width="100px" height="80px" />
             <span class="photo-upload-step">3 <small><?php echo get_str_ordersummary();?></small></span>
         </div>
-		<div class="col-lg-3 col-md-3 tunnel-iconimg">
+		<div class="col-lg-3 col-sm-3 col-xs-6 tunnel-iconimg">
             <img src="<?php echo $upload_dir['baseurl']; ?>/icon/order_confirmed_inactive.png" width="100px" height="80px" />
             <span class="photo-upload-step">4 <small><?php echo get_str_order_complete();?></small></span>
         </div>
